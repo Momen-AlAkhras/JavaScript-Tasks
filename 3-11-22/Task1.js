40e9ab3ab47b6fc81e7a437af81d0741d8637cad
Momen-AlAkhras <momen.alakhras03@gmail.com>
2022-11-03 16:17:51 +0300

    Tasks

---
+++ 3-11-22/Task1.js
@@ -0,0 +1,20 @@
+let mark = 60
+if (mark<50) {
+    document.write(failed);
+}
+else  if (mark>=50 && mark<60)  { 
+    document.write("Your mark is D")
+ }
+ else  if (mark>=60 && mark<70)  { 
+    document.write("Your mark is C")
+}
+else  if (mark>=80 && mark<90)  { 
+    document.write("Your mark is B")
+}
+else  if (mark>=90 && mark<=100)  { 
+    document.write("Your mark is A")
+}
+else{
+    document.write("INVALID")
+}
+    
---
+++ 3-11-22/Task2.js
@@ -0,0 +1,6 @@
+const pi = 50;
+
+
+let area = pi * 50;
+
+document.write (area)
