8d1533dbf0a22d9e9754a44f4afff52e31a556ac
Momen-AlAkhras <momen.alakhras03@gmail.com>
2022-11-06 16:48:01 +0300

    4

---
+++ 6-11-22/Task2.js
@@ -0,0 +1,10 @@
+
+let C=1;
+  for(let i=1;i<=5;i++){
+
+    for (let r=1;r<=i;r++) {
+        document.writeln(C);
+        C+=1
+    }
+   document.write("<br>")
+  }
---
+++ 6-11-22/Task4.js
@@ -0,0 +1,10 @@
+let sum = 0;
+
+for (let i = 0; i < 100; i++) {
+    sum +=i;
+    if (sum>100){
+    document.write(sum)
+    break;
+    }
+  }
+    
